4bff38f1ad6e2f226503cd63b41a4d8f81c303cd
sskansnow <121357735+sskansnow@users.noreply.github.com>
2022-12-24 15:08:35 -0500

    Update streamlit_app.py
    Fixed spellings

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakafst Menu')
+streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spaimch & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
